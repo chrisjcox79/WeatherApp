4f5c7cf22f16960aaef18da246b2c86cd532a16c
san <skysan@gmail.com>
2017-07-28 22:19:39 +0530

    made weather app

--- index.py
+++ index.py
@@ -1,12 +1,31 @@
 from flask import Flask
 import os
+import urllib2
+import json
+import time
 
 app = Flask(__name__)
 
+def get_weather():
+    url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Phillaur&cnt=10&mode=json&units=metric&appid=c0d8761ca979157a45651a5c7f12a6be"
+    response = urllib2.urlopen(url).read()
+    return response
+
 @app.route("/")
 def index():
-    return "Hello World";
+    data = json.loads(get_weather())
+    page = "<html><head><title>My Weather</title></head></html>"
+    page += "<h1>Weather for {}, {}</h1>".format(data.get("city").get("name"),
+            data.get("city").get("country"))
+    for day in data.get("list"):
+        page += "<b> date:</b>{} <b> min:</b>{}<b> max:</b>{} <b>description:</b> {} <br/>".format(
+        time.strftime('%d %B', time.localtime(day.get('dt'))),
+                (day.get("temp").get("min")),
+                day.get("temp").get("max"),
+                day.get("weather")[0].get("description"))
 
+    page +="</body></html>"
+    return page
 @app.route("/goodbye")
 def goodbye():
     return "Goodbye, World"
