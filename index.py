7d08283648732e2381d14160112673ed8e5b5f78
san <skysan@gmail.com>
2017-07-28 21:31:35 +0530

    inital commit

---
+++ index.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello World";
+
+@app.route("/goodbye")
+def goodbye():
+    return "Goodbye, World"
+
+@app.route("/hello/<name>/<int:age>")
+def hello_name(name, age):
+    return "Hello, {} you are {} year old.".format(name, age);
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
+
