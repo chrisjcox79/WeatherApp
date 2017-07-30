3c9d4cf8c56610759fe695e6ae50ccf515382fdb
san <skysan@gmail.com>
2017-07-30 13:13:48 +0530

    pass city from url

--- index.py
+++ index.py
@@ -7,14 +7,18 @@ import time
 
 app = Flask(__name__)
 
-def get_weather():
-    url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Phillaur&cnt=10&mode=json&units=metric&appid=c0d8761ca979157a45651a5c7f12a6be"
+def get_weather(city):
+    url = "http://api.openweathermap.org/data/2.5/forecast/daily?q={}&cnt=10&mode=json&units=metric&appid=c0d8761ca979157a45651a5c7f12a6be".format(city)
     response = urllib2.urlopen(url).read()
     return response
 
 @app.route("/")
-def index():
-    data = json.loads(get_weather())
+@app.route("/<searchCity>")
+def index(searchCity="Philaur"):
+    data = json.loads(get_weather(searchCity))
+    city = data["city"]["name"]
+    country = data["city"]["country"]
+
     forcast_list = []
     for d in data.get("list"):
         day = time.strftime('%d %B', time.localtime(d.get('dt')))
@@ -22,10 +26,7 @@ def index():
         maxi = d.get("temp").get("max")
         desc = d.get("weather")[0].get("description")
         forcast_list.append((day, mini, maxi, desc))
-    return render_template("index.html", forcast_list=forcast_list)
-
-def goodbye():
-    return "Goodbye, World"
+    return render_template("index.html", forcast_list=forcast_list, city=city, country=country)
 
 @app.route("/hello/<name>/<int:age>")
 def hello_name(name, age):
