ed28e15b13b1c8fcfc1a2728baada89443cda165
san <skysan@gmail.com>
2017-08-18 21:20:37 +0530

    added line displaying type of execution environment

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from src import app
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.config['SECRET_KEY'] = 'random'
+    print("Using:{}".format(os.environ['APP_SETTINGS']))
     app.debug=True if "-d" in sys.argv else False
     if "-t" in sys.argv:
         from flask_debugtoolbar import DebugToolbarExtension
